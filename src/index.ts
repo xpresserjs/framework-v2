c6712bd5f14153c64b79bc73a48a9cd178a84d0b
trapcodeio <trapcode.io@gmail.com>
2022-06-09 05:36:48 +0100

    Refactored

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@
 import { Config } from "./types";
 import { Xpresser } from "./xpresser";
 
+/**
+ * ShortHand Initialize Xpresser Function
+ * @param config
+ */
 export function init(config: Config) {
     return new Xpresser(config);
 }
