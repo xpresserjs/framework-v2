bfb6648fd70fea03cae70ff429349ebadc9f27b1
trapcodeio <trapcode.io@gmail.com>
2022-12-05 18:30:35 +0100

    add module functions

--- functions/module.ts
+++ functions/module.ts
@@ -4,10 +4,18 @@ import InXpresserError from "../errors/InXpresserError.js";
  * Import Default Module
  * @param fn
  */
-async function importDefault<T>(fn: () => Promise<{ default: T }>): Promise<T> {
+export async function importDefault<T>(fn: () => Promise<{ default: T }>): Promise<T> {
     const module = await fn();
     if (module.default === undefined) {
         throw new InXpresserError(`Module does not have a default export!`);
     }
     return module.default;
+}
+
+/**
+ * A Function that returns the importDefault function result.
+ * @param fn
+ */
+export async function importDefaultFn<T>(fn: () => Promise<{ default: T }>) {
+    return () => importDefault(fn);
 }
