b21cdf844e48b87d42779bbf98a49bcf13686e38
trapcodeio <trapcode.io@gmail.com>
2022-12-05 07:32:47 +0100

    Test exports

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { init } from "./src/main.js";
+export { defineCommands, CliEngine } from "./src/modules/console/CliEngine.js";
+export { __dirname, __filename } from "./src/functions/path.js";
