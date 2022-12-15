14b077dead9cef8c0cd7df58d3832d0cb2d60837
trapcodeio <trapcode.io@gmail.com>
2022-12-15 15:50:15 +0100

    Refactor: remove main.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-export { init } from "./main.js";
+// Import
+import type { Config } from "./types/configs.js";
+import { Xpresser } from "./xpresser.js";
+
+// Export
 export { defineCommands, CliEngine } from "./modules/console/CliEngine.js";
 export { __dirname, __filename } from "./functions/path.js";
+export { Xpresser };
+
+/**
+ * ShortHand Initialize Xpresser Function
+ * @param config
+ */
+export async function init(
+    config: Config.InitConfig,
+): Promise<Xpresser> {
+    const $ = new Xpresser(config);
+
+    // Add Cli Module
+    await $.modules.useConsoleModule();
+
+    return $;
+}
